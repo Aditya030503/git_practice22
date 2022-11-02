510bb75a7708569a2dffaae6493bbd38edffd11a
Aditya <115468905+Aditya030503@users.noreply.github.com>
2022-11-02 11:58:03 +0530

    add_prime_no.

---
+++ prime_number2.js
@@ -0,0 +1,9 @@
+function checkprime(num){
+    for(let x=1;x<=num;x++){
+    if(num%x==0){
+    retrun true;}
+    else{
+    retrun false}}
+    let ans=checkprime(13)
+    if(ans==true){
+    console.log("prime")}
