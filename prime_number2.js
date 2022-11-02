35f28d0fe45b058ec89efc4bc631843af13e6077
Aditya <115468905+Aditya030503@users.noreply.github.com>
2022-11-02 13:00:21 +0530

    update_by_aditya

--- prime_number2.js
+++ prime_number2.js
@@ -1,9 +1,13 @@
-function checkprime(num){
-    for(let x=1;x<=num;x++){
-    if(num%x==0){
-    retrun true;}
-    else{
-    retrun false}}
+function checkprime(number){
+    for(let x=1;x<=number;x++){
+    if(number%x==0){
+        factor++;}
+        if(factor==2){
+    return true;}
+        }}
     let ans=checkprime(13)
     if(ans==true){
-    console.log("prime")}
+    console.log("it a prime number")}
+    else{
+        console.log("not a prime number")
+    }
